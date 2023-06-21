20c10a825269491c0767a9eb768af21b593a0537
Celeekeybor <122092941+Celeekeybor@users.noreply.github.com>
2023-06-21 02:42:01 -0700

    monty

--- monty.c
+++ monty.c
@@ -1,24 +1,23 @@
 #include "monty.h"
 global_var var_global;
 /**
- * main - driver function for monty program
- * @ac: int num of arguments
- * @av: opcode file
- * Return: 0
+ * main - driver function monty 
+ * @elem1: first element
+ * @elem2: second element
+ * Return: zero
  */
-int main(int ac, char **av)
+int main(int elem1, char **elem2)
 {
 	stack_t *stack;
 
 	stack = NULL;
-	if (ac != 2)
+	if (elem1 != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
-	read_file(av[1], &stack);
-    /* recordar liberar memorias */
+	read_file(elem2[1], &stack);
 	free_dlistint(stack);
 	return (0);
 }
